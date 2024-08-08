e44749c3c8331c4b53b03abc472bc4cf5c6e006c
Simon Ramirez <54188400+snramirez@users.noreply.github.com>
2024-08-08 17:18:18 -0300

    La tabla ya indica si tiene pagos vencidos

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,27 @@
     <v-container>
       <v-card flat>
         <v-card-title class="d-flex align-center pe-2">
-          <v-text-field
-            v-model="search"
-            density="compact"
-            label="Search"
-            prepend-inner-icon="mdi-magnify"
-            variant="solo-filled"
-            flat
-            hide-details
-            single-line
-          ></v-text-field>
+          <v-text-field v-model="search" density="compact" label="Search" prepend-inner-icon="mdi-magnify"
+            variant="solo-filled" flat hide-details single-line></v-text-field>
         </v-card-title>
 
         <v-divider></v-divider>
         <v-data-table v-model:search="search" :headers="headers" :items="data">
-            <template v-slot:item.inicio="{ item }">
-              <span>{{formatDate.formatDate(item.inicio)}}</span>
-            </template>
-            <template v-slot:item.vencimiento="{ item }">
-              <span>{{formatDate.formatDate(item.vencimiento)}}</span>
-            </template>
+          <template v-slot:item.inicio="{ item }">
+            <span>{{ formatDate.formatDate(item.inicio) }}</span>
+          </template>
+
+          <template v-slot:item.vencimiento="{ item }">
+            <span>{{ formatDate.formatDate(item.vencimiento) }}</span>
+          </template>
+
+          <template v-slot:item.certificado="{ item }">
+            <div class="text-end">
+              <v-chip :color="itsDue(item) ? 'red' : 'green'" :text="itsDue(item) ?  'Pagos vencidos' : 'Pagos al Dia'"
+                class="text-uppercase" size="small" label>
+              </v-chip>
+            </div>
+          </template>
         </v-data-table>
 
 
@@ -47,48 +48,28 @@ export default {
         { title: "Estado", value: "estado" },
         { title: "Inicio", value: "inicio" },
         { title: "Vencimiento", value: "vencimiento" },
-        { title: "Certificado", value: "" },
-      ],
-
-      items: [
-        {
-          name: "Nebula GTX 3080",
-          image: "1.png",
-          price: 699.99,
-          rating: 5,
-          stock: true,
-        },
-        {
-          name: "Galaxy RTX 3080",
-          image: "2.png",
-          price: 799.99,
-          rating: 4,
-          stock: false,
-        },
-        {
-          name: "Orion RX 6800 XT",
-          image: "3.png",
-          price: 649.99,
-          rating: 3,
-          stock: true,
-        },
-        {
-          name: "Vortex RTX 3090",
-          image: "4.png",
-          price: 1499.99,
-          rating: 4,
-          stock: true,
-        },
-        {
-          name: "Cosmos GTX 1660 Super",
-          image: "5.png",
-          price: 299.99,
-          rating: 4,
-          stock: false,
-        },
+        { title: "Certificado", value: "certificado" },
       ],
     };
   },
+  methods: {
+    //recorre el pliego y devuelve true si algun control con fecha vencida no fue ejecutado
+    itsDue(pliego) {
+      //si algun renglon tiene algun control vencido devuelve true.
+      return pliego.renglon.some(renglon => {
+        return renglon.orden_compra.control.some(this.vencido)
+      });
+
+    
+    },
+
+    vencido(control) {
+      let currentDate = new Date()
+      if ((new Date(control.periodo) < currentDate) && (control.estado != 'EJECUTADO')) {
+        return true
+      }
+    }
+  }
 };
 </script>
 
--- utils/formatText.js
+++ utils/formatText.js
@@ -5,12 +5,12 @@ moment.locale("es");
 const ctrl = {}
 
 ctrl.formatDate = (date) => {
-    console.log(date)
     if (!date) return;
     let format = moment.utc(date).format("L");
-    console.log(format)
     return format
 }
+
+
 
 ctrl.test = () => {
     return 'hola'
