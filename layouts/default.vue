586ad7ad156cd4f9175902c728b6314d93e7c7ed
snramirez <54188400+snramirez@users.noreply.github.com>
2024-08-15 21:51:13 -0300

    layaout navbar and drawer

---
+++ layouts/default.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <header>
+      <v-layout>
+        <v-app-bar>
+          <v-app-bar-nav-icon
+            @click.stop="drawer = !drawer"
+          ></v-app-bar-nav-icon>
+          <v-app-bar-title>BA Licitaciones</v-app-bar-title>
+        </v-app-bar>
+      
+
+      <v-navigation-drawer v-model="drawer">
+        <v-list-item>
+          <v-list-item-title>Menu</v-list-item-title>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list>
+            <v-list-item>
+                <v-list-item-title>
+                    Dashboard
+                </v-list-item-title>
+                <template v-slot:prepend>
+                    <v-icon icon="mdi-view-dashboard"></v-icon>
+                </template>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-title>
+                    Licitaciones
+                </v-list-item-title>
+                <template v-slot:prepend>
+                    <v-icon icon="mdi-form-select"></v-icon>
+                </template>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-title>
+                    Certificaciones
+                </v-list-item-title>
+                <template v-slot:prepend>
+                    <v-icon icon="mdi-file-certificate"></v-icon>
+                </template>
+            </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
+    </header>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
