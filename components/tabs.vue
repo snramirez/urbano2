8de21a5ec4291775a8ddc4a8188345a060b66a34
Simon Ramirez <54188400+snramirez@users.noreply.github.com>
2024-08-30 14:47:04 -0300

    Continuacion vista pliego completo

---
+++ components/tabs.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <v-tabs v-model="tab">
+            <v-tab value="1">Renglon 1</v-tab>
+            <v-tab value="2">Renglon 2</v-tab>
+            <v-tab value="3">Renglon 3</v-tab>
+        </v-tabs>
+
+        <v-tabs-window v-model="tab">
+            <v-tabs-window-item value="1">
+                <v-card>
+                    <v-row class="my-2 mx-2">
+                        <v-col cols="6">
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                        </v-col>
+
+                        <v-col cols="6">
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                            <p>Info Renglon</p>
+                        </v-col>
+                    </v-row>
+                    <tabsVertical/>
+                </v-card>
+
+
+            </v-tabs-window-item>
+            <v-tabs-window-item value="2">
+                <v-row class="my-2 mx-2">
+                    <v-col cols="6">
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                    </v-col>
+
+                    <v-col cols="6">
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                    </v-col>
+                </v-row>
+            </v-tabs-window-item>
+            <v-tabs-window-item value="3">
+                <v-row class="my-2 mx-2">
+                    <v-col cols="6">
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                    </v-col>
+
+                    <v-col cols="6">
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                        <p>Info Renglon</p>
+                    </v-col>
+                </v-row>
+            </v-tabs-window-item>
+        </v-tabs-window>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            tab: null
+        }
+    },
+    props:{
+        renglon: Array,
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/tabsVertical.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="d-flex flex-row">
+        <v-tabs v-model="tab" direction="vertical">
+            <v-tab value="1">Oferta 1</v-tab>
+            <v-tab value="2">Oferta 2</v-tab>
+            <v-tab value="3">Oferta 3</v-tab>
+        </v-tabs>
+
+        <v-tabs-window v-model="tab">
+            <v-tabs-window-item value="1">
+                <v-card flat>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi quibusdam optio nemo vero adipisci deleniti quisquam esse aliquid hic veniam, eaque molestias illum autem magni voluptas ea aspernatur consectetur at? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sit ipsum corporis quos ullam ab fugit exercitationem maiores, laborum beatae earum quis tempore cum. Officia aliquam nulla fugiat dolorum voluptates!</p>
+                </v-card>
+            </v-tabs-window-item>
+
+            <v-tabs-window-item value="2">
+                <v-card>
+                    <p>Cosas de Ofertas</p>
+                </v-card>
+            </v-tabs-window-item>
+
+            <v-tabs-window-item value="3">
+                <v-card>
+                    <p>Cosas de Ofertas</p>
+                </v-card>
+            </v-tabs-window-item>
+        </v-tabs-window>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        tab: null
+    })
+}
+</script>
+
+<style lang="scss" scoped></style>
