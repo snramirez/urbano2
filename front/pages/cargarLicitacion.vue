e53cb7dadd4a4eba9bef03073338bd8f04486fcc
Simon Ramirez <54188400+snramirez@users.noreply.github.com>
2025-06-19 14:35:07 -0300

    reorganizacion de codigo
    se reorganizo codigo para mejor visibilidad, sin cambios funcionales

--- front/pages/cargarLicitacion.vue
+++ front/pages/cargarLicitacion.vue
@@ -126,112 +126,6 @@
       :renglones="data.renglon"
     />
 
-    <!-- <v-btn @click="addOC">Nueva Orden de Compra</v-btn>
-
-    <v-expansion-panels v-model="panelOC" multiple>
-      <v-expansion-panel v-for="(OC, index) in data.orden_compra" :key="index">
-        <v-expansion-panel-title>
-          Orden de Compra {{ index + 1 }}
-        </v-expansion-panel-title>
-        <v-expansion-panel-text>
-          <v-row>
-            <v-col cols="12" md="3">
-              <v-text-field variant="outlined" label="Numero de Orden" v-model="OC.num_orden"></v-text-field>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-select
-                  v-model="OC.tipo"
-                  :items="tipoOC"
-                  label="Tipo Orden"
-                  required
-                  variant="outlined"
-                ></v-select>
-            </v-col>
-            <v-col cols="12" md="3">
-              <currency-field label="Monto Original" v-model="OC.monto"></currency-field>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-select
-                  v-model="OC.destinatario"
-                  multiple
-                  chips
-                  :item-props="itemProps"
-                  :items="contratistas"
-                  label="Contratista"
-                  required
-                  variant="outlined"
-                ></v-select>
-            </v-col>
-          </v-row>
-
-          <v-row>
-            <v-col cols="12" md="3">
-              <v-select
-                  v-model="OC.origen"
-                  :items="origenOC"
-                  label="Origen"
-                  required
-                  variant="outlined"
-                ></v-select>
-            </v-col>
-
-            <v-col v-if="OC.origen === 'AMPLIATORIA'" cols="12" md="3">
-              <v-select
-                  v-model="OC.ampliatoria_origen"
-                  :items="getAmpliatoria()"
-                  label="Ampliatoria origen"
-                  required
-                  clearable
-                  variant="outlined"
-                ></v-select>
-            </v-col>
-
-            <v-col v-if="OC.origen === 'PRORROGA'" cols="12" md="3">
-              <v-select
-                  v-model="OC.prorroga_origen"
-                  :items="getPrroroga()"
-                  label="Prorroga origen"
-                  required
-                  clearable
-                  variant="outlined"
-                ></v-select>
-            </v-col>
-
-            <v-col v-if="OC.origen === 'RENGLON'" cols="12" md="3">
-              <v-select
-                  v-model="OC.renglon_origen"
-                  :items="getRenglon()"
-                  label="Renglon origen"
-                  required
-                  clearable
-                  variant="outlined"
-                ></v-select>
-            </v-col>
-
-            <v-col cols="12" md="3">
-              <currency-field variant="outlined" :model-value="getMontoTotal(OC)" readonly label="Monto Total"></currency-field>
-            </v-col>
-          </v-row>
-
-
-          <v-row>
-            <v-col cols="6">
-              <prorroga :data=OC.prorroga />
-            </v-col>
-            <v-col cols="6">
-              <ampliatoria v-model:ampliatoria="OC.ampliatoria"></ampliatoria>
-            </v-col>
-          </v-row>
-
-          <v-row>
-            <v-col cols="12">
-              <control :orden_compra=OC />
-            </v-col>
-          </v-row>
-        </v-expansion-panel-text>
-      </v-expansion-panel>
-    </v-expansion-panels> -->
-
   </v-container>
 </template>
 
@@ -241,10 +135,7 @@ import { ref } from 'vue'
 
 
 const panel = ref([])
-const panelOC = ref([])
 const data = ref(datos.empyData)
-const tipoOC = ["ABIERTA", "CERRADA"]
-const origenOC = ["RENGLON", "AMPLIATORIA", "PRORROGA"]
 const contratistas = ref([
   {
     razon_social: "Carlos SRL",
@@ -260,62 +151,6 @@ const contratistas = ref([
   },
 ])
 
-
-function addOC() {
-  data.orden_compra.push({
-    num_orden: '',
-    monto: 0,
-    tipo: '',
-    destinatario: null,
-    origen:'',
-    ampliatoria_origen: '',
-    prorroga_origen: '',
-    renglon_origen: '',
-    prorroga: [],
-    ampliatoria: [],
-    control:[]
-  });
-}
-
-function itemProps(item) {
-  return {
-    title: item.razon_social,
-    subtitle: item.cuit,
-  };
-}
-
-function getAmpliatoria(){
-  let listAmp = []
-  data.orden_compra.forEach(OC => {
-    OC.ampliatoria.forEach(amp => {
-      listAmp.push(amp.acta)
-    })
-  })
-  return listAmp
-}
-
-function getPrroroga(){
-  let listProrroga = []
-  data.orden_compra.forEach(OC => {
-    OC.prorroga.forEach(pro => {
-      listProrroga.push(pro.acta)
-    })
-  })
-  return listProrroga
-}
-
-function getRenglon(){
-  let listRenglon = []
-  data.renglon.forEach(renglon => listRenglon.push(renglon.descripcion))
-  return listRenglon
-}
-
-function getMontoTotal(orden){
-  let montoTotal = 0
-  orden.ampliatoria.forEach(amp => montoTotal += amp.monto)
-  montoTotal += orden.monto
-  return montoTotal 
-}
 
 </script>
 
